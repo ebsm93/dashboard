d1ed8b49773e6c36d952264ee59357e00139e58f
ebs <ebs.m93@gmail.com>
2022-03-01 20:27:40 -0800

    initial dist subtree commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import './style.css';
 
-import documentIcon from './images/document_icon.svg';
+import personImage from './images/person_portrait_wt.svg';
+import documentIcon from './images/document_icon_wt.svg';
 import emailIcon from './images/email.svg';
+import ghIcon from './images/github_wt.svg';
+import ytImage from './images/yt_icon_wt.svg';
 
 
 
+//DOM
 
+const aboutMe = document.querySelector('#about');
 const tech = document.querySelector('#tech');
 const general = document.querySelector('#general');
 const contact = document.querySelector('#contact');
+const ghub = document.querySelector('#github');
+const yt = document.querySelector('#yt');
 
-
+const aboutIcon = new Image();
+aboutIcon.src = personImage;
+aboutIcon.classList.add('icon');
+aboutMe.prepend(aboutIcon);
 
 const docIcon = new Image();
 docIcon.src = documentIcon;
@@ -22,12 +32,20 @@ tech.prepend(docIcon);
 general.prepend(docIconCopy);
 
 
-
 const contactIcon = new Image();
 contactIcon.src = emailIcon;
 contactIcon.classList.add('icon');
 contact.prepend(contactIcon);
 
+const ghubIcon = new Image();
+ghubIcon.src = ghIcon;
+ghubIcon.classList.add('icon');
+ghub.prepend(ghubIcon);
+
+const ytIcon = new Image();
+ytIcon.src = ytImage;
+ytIcon.classList.add('icon');
+yt.prepend(ytIcon);
 
 
 

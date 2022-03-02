bc6fb8b381551a0b176bc94cb5f6a41c7042fd2f
ebs <ebs.m93@gmail.com>
2022-03-01 16:50:05 -0800

    layout set, styling theme mostly set, no card content yet

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import './style.css';
+
+import documentIcon from './images/document_icon.svg';
+import emailIcon from './images/email.svg';
+
+
+
+
+const tech = document.querySelector('#tech');
+const general = document.querySelector('#general');
+const contact = document.querySelector('#contact');
+
+
+
+const docIcon = new Image();
+docIcon.src = documentIcon;
+const docIconCopy = new Image();
+docIconCopy.src = documentIcon;
+docIcon.classList.add('icon');
+docIconCopy.classList.add('icon');
+tech.prepend(docIcon);
+general.prepend(docIconCopy);
+
+
+
+const contactIcon = new Image();
+contactIcon.src = emailIcon;
+contactIcon.classList.add('icon');
+contact.prepend(contactIcon);
+
+
+
+
+
 console.log(`testing 123`);
